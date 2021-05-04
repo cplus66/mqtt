b49bdf7c34d776ae7a5a09b3143e61108cca0bae
Cplus Shen <cplus.shen@gmail.com>
2021-05-04 15:12:02 +0800

    supported retain message.

--- mqtt-pub.sh
+++ mqtt-pub.sh
@@ -1,4 +1,15 @@
 #!/bin/bash -xe
+# Date: May 04, 2021
+# Author: cplus.shen@gmail.com
+# Description: publish a MQTT retain message.
+# HOST, ID and PASS should be declared in ../mqtt.rc
+
 source ../mqtt.rc
 
-mosquitto_pub -t $TOPIC -m "Hello!" -h $HOST -u $ID -P $PASS
+MQTT_FLAGS="-r"
+MSG="http://172.17.5.195/tftp/NCG/jenkins/vega-3500-card-yocto/48036/74/core-image-vega_r48036.wic"
+
+mosquitto_pub -h $HOST -u $ID -P $PASS \
+	$MQTT_FLAGS \
+	-t $TOPIC \
+	-m $MSG
--- mqtt-sub.sh
+++ mqtt-sub.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -xe
+# Date: May 04, 2021
+# Author: cplus.shen@gmail.com
+# Description: subscribe the latest MQTT message.
+# HOST, ID and PASS should be declared in ../mqtt.rc
 
 source ../mqtt.rc
 
-mosquitto_sub -t $TOPIC -h $HOST -u $ID -P $PASS
+MQTT_FLAGS="-C 1"
+mosquitto_sub -h $HOST -u $ID -P $PASS \
+	$MQTT_FLAGS \
+	-t $TOPIC
